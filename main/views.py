5b9c7d9c6f336d3b96db19520cf29bcdf852024d
Korb1t <oles.hobor@gmail.com>
2019-03-21 11:37:48 +0200

    news

--- main/views.py
+++ main/views.py
@@ -41,7 +41,14 @@ def get_area_name(name):
 
 
 def index(request):
-    return render(request, 'index.html')
+    posts = Post.objects.all().order_by('-date')
+    big_post = posts[0]
+    small_post = posts[1:5]
+    ctx = {
+        'big': big_post,
+        'small': small_post,
+    }
+    return render(request, 'index.html', ctx)
 
 
 def blog(request):
