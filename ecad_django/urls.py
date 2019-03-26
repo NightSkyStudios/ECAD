d0851f7b93d1fce08a433dd13f93cb5440513ecf
DigitalG <thesever99@gmail.com>
2019-03-26 17:46:14 +0200

    translation

--- ecad_django/urls.py
+++ ecad_django/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from main import views
 from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = i18n_patterns(
+    path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
     path('equipment/', views.equipment, name='equipment'),
     path('projects/', views.projects, name='projects'),
